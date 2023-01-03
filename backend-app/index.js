8c9942942f2c840e0f7f43117cb5abe84fa8e0a5
Janaki Kangalgouda <42041306+janakigouda@users.noreply.github.com>
2023-01-03 12:39:02 +0530

    backend apis updated

--- backend-app/index.js
+++ backend-app/index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
 
 app.use("/users",userRoute);
 app.use("/userNotice",userNoticesRoute);
